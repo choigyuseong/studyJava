9e541880ec89176e6a7be06f1136abca8bf91752
Gyuseong2 <clitico12@gmail.com>
2025-03-17 15:51:59 +0900

    Basik01

---
+++ src/main/java/org/example/javaBasic01.java
@@ -0,0 +1,53 @@
+package org.example;
+
+// 자료형, if, while, switch
+public class javaBasic01 { // 클래스 이름은 항상 파일 이름과 같다.
+    public static void main(String[] args) {
+// java는 JVM(Java Virtual Machine을 통해서 실행된다. main 메서드를 찾아 실행하면서 java 프로그램이 시작된다.
+//        System.out.println("Hello, World!");
+
+        // 자료형 타입
+//        int age = 25;
+//        double height = 172.2;
+//        boolean isStudent = true;
+//        char grade = 'A';
+//        String name = "Choi";
+        
+        // if 문 예제
+//        if (age >= 19) {
+//            System.out.println("성인");
+//        } else if(age >= 12) {
+//            System.out.println("청소년");
+//        } else {
+//            System.out.println("아이");
+//        }
+
+        // while 문 예제
+//        int birthYear = 0;
+//        while(birthYear != age) {
+//            ++birthYear;
+//        }
+//        System.out.println(birthYear);
+
+        // do while 문 예제
+//        int num = 10;
+//        boolean isNumBigger = true;
+//        do {
+//            System.out.println("It's not false");
+//            --num;
+//            if (num < 10) {
+//                isNumBigger = false;
+//            }
+//        } while (isNumBigger);
+
+        // switch 문 예제
+//        switch (grade) {
+//            case 'A':
+//                System.out.println("A학점");
+//            case 'B':
+//                System.out.println("B학점");
+//            default:
+//                System.out.println("Unkown grade");
+//        }
+    }
+}
